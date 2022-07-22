2bc59bf54384af608fc72ac14ac5b80198e37465
Jhuliana <jholeanasantos@gmail.com>
2022-07-22 15:29:18 -0300

    feat: config app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log('Server ta funfando');
+});
