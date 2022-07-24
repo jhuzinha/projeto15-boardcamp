a28a0a077abd7e3f127df75ba6fd7184ef9bf375
Jhuliana <jholeanasantos@gmail.com>
2022-07-24 15:34:17 -0300

    fix: try and catch in categories

--- src/controllers/categoriesControllers.js
+++ src/controllers/categoriesControllers.js
@@ -2,12 +2,19 @@ import connection from "../database.js";
 
 export async function createCategories(req, res) {
     const { name } = req.body;
-    
-    await connection.query('INSERT INTO categories (name) VALUES ($1)', [name]);
-    res.sendStatus(201);
+    try {
+        await connection.query('INSERT INTO categories (name) VALUES ($1)', [name]);
+        res.sendStatus(201); 
+    } catch (err) {
+        res.sendStatus(500);
+    }
 }
 
 export async function getCategories(req, res) {
-    const categories = await connection.query('SELECT * FROM categories');
-    res.send(categories.rows)
+    try {
+        const categories = await connection.query('SELECT * FROM categories');
+        res.send(categories.rows)
+    } catch (err) {
+        res.sendStatus(500)
+    }
 }
